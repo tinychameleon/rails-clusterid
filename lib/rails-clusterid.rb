89fa5434c26bff1d5ce5b94b19296f0cc2ba14d3
Stephan Tarulli <srt@tinychameleon.com>
2022-02-28 19:14:43 -0800

    Adjust module layout

---
+++ lib/rails-clusterid.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+module ClusterId
+  module Rails
+  end
+end
+
+require_relative "rails-clusterid/version"
+
+module ClusterId::Rails
+  class Error < StandardError; end
+  # Your code goes here...
+end
--- lib/rails-clusterid/version.rb
+++ lib/rails-clusterid/version.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
-module Rails
-  module Clusterid
+module ClusterId
+  module Rails
     VERSION = "0.1.0"
   end
 end
