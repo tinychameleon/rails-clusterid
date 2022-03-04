bd277068992daa899b6632cf2d29316e42f89316
Stephan Tarulli <srt@tinychameleon.com>
2022-03-03 22:04:33 -0800

    Add ClusterId migrations

--- test/dummy/db/migrate/20220304032223_create_users.rb
+++ test/dummy/db/migrate/20220304032223_create_users.rb
@@ -1,7 +1,6 @@
 class CreateUsers < ActiveRecord::Migration[7.0]
   def change
-    create_table :users do |t|
-      t.clusterid :id
+    create_table :users, id: :clusterid do |t|
       t.string :name
 
       t.timestamps
--- test/dummy/db/migrate/20220304032352_create_settings.rb
+++ test/dummy/db/migrate/20220304032352_create_settings.rb
@@ -1,10 +1,9 @@
 class CreateSettings < ActiveRecord::Migration[7.0]
   def change
-    create_table :settings do |t|
-      t.clusterid :id
+    create_table :settings, id: :clusterid do |t|
       t.string :name
       t.string :value
-      t.references :user, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true, type: :clusterid
 
       t.timestamps
     end
