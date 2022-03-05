431e374332f24c7d07dab59be74e4f25a7763a88
Stephan Tarulli <srt@tinychameleon.com>
2022-03-05 12:05:12 -0800

    Add small user resource to dummy app

--- test/dummy/app/controllers/users_controller.rb
+++ test/dummy/app/controllers/users_controller.rb
@@ -1,4 +1,30 @@
 class UsersController < ApplicationController
   def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    user = User.create(params.permit(:name))
+    if user.save
+      redirect_to users_path
+    else
+      redirect_to new_user_path(user)
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
   end
 end
---
+++ test/dummy/config/initializers/clusterid.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+#Rails.application.config.clusterid do |c|
+#  c.generators = {
+#    default: ClusterId::V1::Generator.new,
+#    another: ClusterId::V1::Generator.new,
+#  }
+#  c.default_generator = :default
+#end
