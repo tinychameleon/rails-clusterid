ffefb47acfdc1f9c9d721be310863ebf0d8616dc
Stephan Tarulli <srt@tinychameleon.com>
2022-03-18 20:46:42 -0700

    Update the gem require statements

--- lib/rails-clusterid.rb
+++ lib/rails-clusterid.rb
@@ -3,13 +3,18 @@
 module ClusterId
   module Rails
     DATA_TYPE = :clusterid
+    DEFAULT_GENERATOR_NAME = :default
+    BYTE_LENGTH = 16
+    CROCKFORD32_LENGTH = 26
+
+    class Error < StandardError; end
+
+    class NotAGeneratorError < Error; end
+
+    def self.clusterid_primary_keys?
+      DATA_TYPE == ::Rails.application.config.generators.options.dig(:active_record, :primary_key_type)
+    end
   end
-end
-
-module ClusterId::Rails
-  class Error < StandardError; end
-
-  class NotAGeneratorError < Error; end
 end
 
 require "clusterid"
@@ -17,5 +22,8 @@ require "crockford32"
 
 require_relative "rails-clusterid/version"
 require_relative "rails-clusterid/config"
-require_relative "rails-clusterid/datatype"
+require_relative "rails-clusterid/application_config"
+require_relative "rails-clusterid/type"
+require_relative "rails-clusterid/model"
+require_relative "rails-clusterid/active_record/schema_statements"
 require_relative "rails-clusterid/railtie"
